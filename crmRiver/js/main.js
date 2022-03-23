0c3e0b60ac2a3f6de3b1d8a00660d05148a63e5e
andrestr93 <andres.torcuato93@hotmail.com>
2022-03-23 11:48:08 +0100

    Copia de respaldo 23/03/2022

--- crmRiver/js/main.js
+++ crmRiver/js/main.js
@@ -1,20 +1,7 @@
 
 
 
-/*
-let peticion= "";
-let nombre = "";
-let apellidos= "";
-let email = "";
-let telefono = "";
-let tecnologia = "";
-let document1= "";
-let document2 = "";
-let documento3 = "";
-
-*/
-
-
+let dragged;
 
 function create_Petition() {
 
@@ -83,41 +70,52 @@ function getInformation() {
 }
 
 
-function dragStart(event) { // cuando vamos a arrastrar
+/* events fired on the draggable target */
+document.addEventListener("drag", function(event) {
 
-    event.dataTransfer.setData("sugerencia", event.target.id)
+}, false);
 
+document.addEventListener("dragstart", function(event) {
+  // store a ref. on the dragged elem
+  dragged = event.target;
+  // make it half transparent
+  event.target.style.opacity = .5;
+}, false);
 
+document.addEventListener("dragend", function(event) {
+  // reset the transparency
+  event.target.style.opacity = "";
+}, false);
 
+/* events fired on the drop targets */
+document.addEventListener("dragover", function(event) {
+  // prevent default to allow drop
+  event.preventDefault();
+}, false);
 
-}
+document.addEventListener("dragenter", function(event) {
+  // highlight potential drop target when the draggable element enters it
+  if (event.target.className == "container") {
+    event.target.style.background = "purple";
+  }
 
+}, false);
 
+document.addEventListener("dragleave", function(event) {
+  // reset background of potential drop target when the draggable element leaves it
+  if (event.target.className == "container") {
+    event.target.style.background = "";
+  }
 
-function onDragOver(event) { //mientras se esta arrastrando
+}, false);
 
-    event.preventDefault();
-}
-
-
-
-
-function onDrop(event) { // cuando terminamos de soltar 
-    let contenedor = document.getElementById("caja2")
-    let contenedor2 = document.getElementById("caja3")
-    let contenedor3 = document.getElementById("caja4")
-    let contenedor4 = document.getElementById("caja5")
-    let contenedor5 = document.getElementById("caja2")
-
-    let contacto = document.getElementById("caja")
-
-    event.preventDefault
-
-    contenedor.appendChild(contacto)
-
-
-
-
-
-
-}
+document.addEventListener("drop", function(event) {
+  // prevent default action (open as link for some elements)
+  event.preventDefault();
+  // move dragged elem to the selected drop target
+  if (event.target.className == "container") {
+    event.target.style.background = "";
+    dragged.parentNode.removeChild( dragged );
+    event.target.appendChild( dragged );
+  }
+}, false);
