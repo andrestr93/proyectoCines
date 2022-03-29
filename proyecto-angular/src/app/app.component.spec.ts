7c33e1be7ab7763cc15b4837a5de52625c3fbf5f
andrestr93 <andres.torcuato93@hotmail.com>
2022-03-29 09:35:54 +0200

    copia de respaldo 29-03-2022

--- proyecto-angular/src/app/app.component.spec.ts
+++ proyecto-angular/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('proyecto-angular app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hola mundo desde HTML');
   });
 });
--- proyecto-angular/src/app/app.component.ts
+++ proyecto-angular/src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'proyecto-angular';
+  title = 'esto es con la interpolacion ';
+
+
+
+   prueba(){
+
+    console.log("esto es una prueba con una funcion , mostrando un console log")
+  };
+
+  pruebaPelicula(){
+
+     let pelicula = {
+
+      nombre: "Avatar",
+      fecha_lanzamiento : new Date()
+
+
+    };
+
+
+    return pelicula;
+ 
+  }
+  
 }
+
